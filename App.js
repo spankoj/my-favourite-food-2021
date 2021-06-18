559e68689b35ef9716005e1d2536540382f31cf6
spankoj <spanyesz@gmail.com>
2021-06-18 14:11:06 +0200

    Add project files pkges

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer></NavigationContainer>
+    <Fragment>
+      <StatusBar backgroundColor={'#ff9100'} translucent={true} style="light" />
+      <View style={styles.container}>
+        <Text style={styles.text}>Test SJ</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Fragment>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff9100',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
   },
 });
